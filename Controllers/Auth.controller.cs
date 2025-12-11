6c823289608dc054d714a84d5c7575ac8fd71426
Sabari S <incrediblesabari02@gmail.com>
2025-12-11 14:54:19 +0530

    auth methods

--- Controllers/Auth.controller.cs
+++ Controllers/Auth.controller.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Wayplot_Backend.Constants;
 using Wayplot_Backend.DTOs;
 using Wayplot_Backend.Services;
 
 namespace Wayplot_Backend.Controllers
 {
     [ApiController]
-    [Route("[controller]/login")]
+    [Route("[controller]")]
     public sealed class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
@@ -15,8 +16,41 @@ namespace Wayplot_Backend.Controllers
             _authService = service;
         }
 
-        [HttpPost]
+        [HttpGet("get-auth-types")]
+        public IActionResult GetAuthTypes()
+        {
+            var values = Enum.GetNames(typeof(AuthType)).ToList();
+            return Ok(values);
+        }
+
+
+        [HttpGet("get-user-roles")]
+        public IActionResult GetUserRoles()
+        {
+            var values = Enum.GetNames(typeof(UserRole)).ToList();
+            return Ok(values);
+        }
+
+
+        [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            string email = request.Email;
+            string password = request.Password;
+
+            LoginResponseDto res = await _authService.Login(email, password);
+            if (res.IsSuccess)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest(res);
+            }
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Signup([FromBody] LoginRequestDto request)
         {
             string email = request.Email;
             string password = request.Password;
--- DTOs/User.dto.cs
+++ DTOs/User.dto.cs
@@ -5,14 +5,11 @@ namespace Wayplot_Backend.DTOs
 {
     public class UserUpdateDTO
     {
-        public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public AuthType? AuthType { get; set; }
         public UserRole? Role { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
         public UserStatus? Status { get; set; }
         public List<string>? Scopes { get; set; }
         public string GetScopesJsonFromArray() => JsonSerializer.Serialize(Scopes);
